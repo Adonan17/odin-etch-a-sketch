8fa33331aec2b0f1aeb98496ed2bdc9a33571b70
Adonan17 <adnaneouassari@gmail.com>
2023-12-13 10:50:36 +0100

    finishing first version

--- public/js/main.js
+++ public/js/main.js
@@ -1,41 +1,30 @@
-// var container = document.querySelector(".container")
-
-
-
-// button.addEventListener('click', () => {
-//     container.innerHTML = ""
-//     var input = +prompt("how many rows and columns do you want?")
-//     if (input > 50){
-//         alert("too high, stay under 50");
-//     } else {
-//         for (let i = 0; i < (input*input); i++) {
-//             container.innerHTML += "<div class='gridItem'><div>";
-//         }
-//     }
-// })
-
 const button = document.querySelector("button")
 const container = document.querySelector(".container");
 
-function makeRows(rows, cols) {
+function createGrid(rows, cols) {
     container.innerHTML = ""
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows * cols); c++) {
     let gridItem = document.createElement("div");
     container.appendChild(gridItem).className = "gridItem";
+    let placeColor = document.querySelectorAll(".gridItem");
+
+    placeColor.forEach(item => {
+        item.addEventListener("mouseover", () => {
+            item.style.backgroundColor = "black";
+            item.style.border = "1px solid #464646"
+        });
+    });
   };
 };
+
 
 button.addEventListener('click', () => {
     var input = parseInt(prompt("how many rows and columns do you want?"))
     if (input > 50){
         alert("too high, stay under 50");
     } else {
-        makeRows(input, input);
+        createGrid(input, input);
     }
 })
-
-gridItem.addEventListener('onmouseover', function(){
-    gridItem.style.backgroundColor = 'black';
-});
